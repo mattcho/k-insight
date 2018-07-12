65d41608f9f0d4e95a5161da03a8e01bd55462ae
stoas-seung <stoas.seung@gmail.com>
2018-07-12 23:49:36 +0900

    first commit & push seung

---
+++ kinsight.spring.scheduler/src/main/java/kinsight/spring/scheduler/SApp.java
@@ -0,0 +1,12 @@
+package kinsight.spring.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SApp.class, args);
+	}
+}
