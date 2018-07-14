712a44b3c7c8e873b3e29f3e888d58dcb093c139
seung <seung@218.38.137.28>
2018-07-14 21:53:20 +0900

    finsight scraping library

---
+++ ex/src/main/java/finsight/lib/ex/App.java
@@ -0,0 +1,15 @@
+package finsight.lib.ex;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
